b4aa6754756e3f20566e443b329a76ab9c732876
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-10-14 14:30:39 +0200

    initial commit

---
+++ bin/docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+if [ -f tmp/pids/server_1.pid ]; then
+  rm tmp/pids/server_1.pid
+fi
+
+if [ -f tmp/pids/server_2.pid ]; then
+  rm tmp/pids/server_2.pid
+fi
+
+bundle exec rails s -b 0.0.0.0 -e development
+
