9ea5fac5b0d856ffb9fbe3007772497cc2dc7c45
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-11-04 15:49:46 +0300

    added allowed hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,9 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.hosts << "localhost"
+  config.hosts << "127.0.0.1"
+  config.hosts << "application"
+  config.hosts << "balancer"
+  config.hosts << "80.93.62.150"
 end
