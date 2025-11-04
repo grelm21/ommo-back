dc7dc8693472b75f96c186d1dad5d5d031eefb9d
Grigory Mitrofanov <grelm.wolski@gmail.com>
2025-11-04 12:46:36 +0300

    added credentials

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,9 @@ module OmmoBack
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.hosts << "application"
+    config.hosts << "balancer"
+    config.hosts << "localhost"
   end
 end
